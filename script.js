1a9cd47eb4cacf179d8567e333ebfb06f315a4e2
Damien <boenapfel.damien@yahoo.fr>
2024-11-24 12:27:14 +0100

    maj

--- script.js
+++ script.js
@@ -30,10 +30,17 @@ function lesCadeaux(){
         board.appendChild(cadeaux) // DIV CADEAUX DANS ID BOARD
     
         cadeaux.addEventListener("click", function() {  // INTERRUPTEUR .CLICKED
-            this.classList.toggle("clicked");
+            if (popupOuvert || cadeaux.classList.contains("ouvert")) {
+                return; // PAS POSSIBLE DE CLICK SI "POPUP" OUVERT OU SI "CADEAUX" OUVERT
+            } else {
+                cadeaux.classList.toggle("clicked"); // INTERRUPTEUR OUVERT/FERMER DE "CADEAUX"
+                cadeaux.classList.add("ouvert"); // INDIQUE QUE LE "CADEAUX" EST OUVERT
 
-            const randomMessage = getRandomMessage();  // CREE "randomMessage" à partir de la fonction qui utilise "messages"
-            afficherPopup("SURPRISE ! <br><br>" + randomMessage);
+                const randomMessage = getRandomMessage();  // CREE "randomMessage" à partir de la fonction qui utilise "messages"
+                afficherPopup("SURPRISE ! <br><br>" + randomMessage);
+            }
+            
+
         })
     }
 }
@@ -41,18 +48,21 @@ function lesCadeaux(){
 lesCadeaux()
 
 // POP UP //
+let popupOuvert = false; // CREE VARIABLE POPUPOUVERT INITIALEMENT FERMER
+
 function afficherPopup(message) {
     const popup = document.getElementById("popup");
     const popupMessage = document.getElementById("popupMessage");
     popup.style.display = "flex"; // AFFICHE POP UP
 
     popupMessage.innerHTML = message; // POUR UTILISER LE RETOUR A LA LIGNE (plutot que textContent)
+    popupOuvert = true; // INDIQUE POPUP OUVERT
+    }
 
-}
-
-const btnClose = document.getElementById("btnClose"); // AU CLIQUE POPUP NONE
-btnClose.addEventListener("click", function(){
+    const btnClose = document.getElementById("btnClose"); // AU CLIQUE POPUP NONE
+    btnClose.addEventListener("click", function(){
     popup.style.display = "none";
+    popupOuvert = false; // INDIQUE POPUP FERMER
 })
 
 
